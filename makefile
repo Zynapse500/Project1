5d51e34dbc01a9e6ae67cd012c2ce10b0ec1c3ef
Christofer Nolander <christofer.nolander@gmail.com>
2016-08-05 14:50:18 +0200

    interpolated triangles!!!

--- makefile
+++ makefile
@@ -6,13 +6,14 @@ EXE = main.exe
 
 INCLUDES = -I ./include -I ../glfw-3.2.bin.WIN32/include -I ..\glew-2.0.0\include
 
-LIBRARIES = -L ../glfw-3.2.bin.WIN32/lib-mingw-w64 -L ..\glew-2.0.0\lib\Release\Win32
+LIBRARIES = -L ../glfw-3.2.bin.WIN32/lib-mingw-w64 -L ..\glew-2.0.0\lib
 
-LIBS = -lopengl32 -lglu32 -lglew32 -lglfw3 
+LIBS = -lglew32  -lopengl32 -lglu32 -lglfw3 
 
 $(EXE): $(CPPFILES)
 	@echo Compiling $(CPPFILES)...
-	$(CC) -o $@ $^ $(INCLUDES) $(LIBRARIES) $(LIBS)
+	@$(CC) -o $@ $^ $(INCLUDES) $(LIBRARIES) $(LIBS) 
+	@#-mwindows
 	@echo Done!
 
 .PHONY: $(EXE)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 //Standard includes
 #include <stdio.h>
+#include <math.h>
 #include <conio.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+//My includes
+#include "Shader.h"
 
 //OpenGL includes
 //GLEW
@@ -9,6 +16,10 @@
 #include <GLFW/glfw3.h>
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+
+
+
 
 int main()
 {
@@ -32,7 +43,7 @@ int main()
 
 	glfwSetKeyCallback(window, key_callback);
 	
-	//glewExperimental = GL_TRUE;
+	glewExperimental = GL_TRUE;
 	if(glewInit() != GLEW_OK)
 	{
 		printf("Failed to initialize GLEW\n");
@@ -45,6 +56,42 @@ int main()
 	glViewport(0, 0, width, height);
 
 
+	Shader ourShader("shaders/vertexShader.vert", "shaders/fragmentShader.frag");
+
+
+	//Vertices
+	GLfloat vertices[] = {
+    // Positions         // Colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // Bottom Right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // Bottom Left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // Top 
+	};    
+
+	GLuint VBO, VAO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		//position attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
+
+		//color attribute
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
+		glEnableVertexAttribArray(1);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+	//Drawing Mode
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	//Game loop
 	while(!glfwWindowShouldClose(window))
 	{
 		//Check and call events
@@ -54,14 +101,22 @@ int main()
 		glClearColor(0.2,0.3,0.3,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//Activate the shader
+		ourShader.Use();
+
+		//draw the triangle
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
 		//Swap the buffers
 		glfwSwapBuffers(window);
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
-
-
 	return 0;
 }
 
@@ -72,3 +127,23 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
 }
+
+std::string getFromFile(std::string path)
+{
+	std::string ret;
+	std::ifstream ifs(path.c_str());
+	if(ifs)
+	{
+		std::string line;
+		while(getline(ifs, line))
+		{
+			ret = ret + line + "\n";
+		}
+		ifs.close();
+	}
+	else
+	{
+		return "";
+	}
+	return ret;
+}	
